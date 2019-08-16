4ab93a4e66d5b740df0af3b20b627de327b8a903
CryptAxe <cryptaxe@gmail.com>
2019-08-15 19:17:47 -0700

    some rough comments

--- include/abstractions/script/script.hpp
+++ include/abstractions/script/script.hpp
@@ -15,16 +15,16 @@ namespace abstractions::script {
         virtual N length() const = 0;
         virtual void write(ostream&) const = 0;
         bytes compile() const;
-        
+
         operator bytes() const {
             return compile();
         }
-        
+
         enum op : byte {
-            // push value
+            // OP_0 and OP_FALSE push the bytes 0x00 onto the stack
             OP_0 = 0x00,
             OP_FALSE = OP_0,
-            
+
             OP_PUSHSIZE1 = 0x01,
             OP_PUSHSIZE2 = 0x02,
             OP_PUSHSIZE3 = 0x03,
@@ -34,8 +34,8 @@ namespace abstractions::script {
             OP_PUSHSIZE7 = 0x07,
             OP_PUSHSIZE8 = 0x08,
             OP_PUSHSIZE9 = 0x09,
-            
-            OP_PUSHSIZE10 = 0x0a, 
+
+            OP_PUSHSIZE10 = 0x0a,
             OP_PUSHSIZE11 = 0x0b,
             OP_PUSHSIZE12 = 0x0c,
             OP_PUSHSIZE13 = 0x0d,
@@ -45,7 +45,7 @@ namespace abstractions::script {
             OP_PUSHSIZE17 = 0x11,
             OP_PUSHSIZE18 = 0x12,
             OP_PUSHSIZE19 = 0x13,
-            
+
             OP_PUSHSIZE20 = 0x14,
             OP_PUSHSIZE21 = 0x15,
             OP_PUSHSIZE22 = 0x16,
@@ -56,7 +56,7 @@ namespace abstractions::script {
             OP_PUSHSIZE27 = 0x1b,
             OP_PUSHSIZE28 = 0x1c,
             OP_PUSHSIZE29 = 0x1d,
-            
+
             OP_PUSHSIZE30 = 0x1e,
             OP_PUSHSIZE31 = 0x1f,
             OP_PUSHSIZE32 = 0x20,
@@ -67,7 +67,7 @@ namespace abstractions::script {
             OP_PUSHSIZE37 = 0x25,
             OP_PUSHSIZE38 = 0x26,
             OP_PUSHSIZE39 = 0x27,
-            
+
             OP_PUSHSIZE40 = 0x28,
             OP_PUSHSIZE41 = 0x29,
             OP_PUSHSIZE42 = 0x2a,
@@ -78,7 +78,7 @@ namespace abstractions::script {
             OP_PUSHSIZE47 = 0x2f,
             OP_PUSHSIZE48 = 0x30,
             OP_PUSHSIZE49 = 0x31,
-            
+
             OP_PUSHSIZE50 = 0x32,
             OP_PUSHSIZE51 = 0x33,
             OP_PUSHSIZE52 = 0x34,
@@ -89,7 +89,7 @@ namespace abstractions::script {
             OP_PUSHSIZE57 = 0x39,
             OP_PUSHSIZE58 = 0x3a,
             OP_PUSHSIZE59 = 0x3b,
-            
+
             OP_PUSHSIZE60 = 0x3c,
             OP_PUSHSIZE61 = 0x3d,
             OP_PUSHSIZE62 = 0x3e,
@@ -100,68 +100,118 @@ namespace abstractions::script {
             OP_PUSHSIZE67 = 0x43,
             OP_PUSHSIZE68 = 0x44,
             OP_PUSHSIZE69 = 0x45,
-            
+
             OP_PUSHSIZE70 = 0x46,
             OP_PUSHSIZE71 = 0x47,
             OP_PUSHSIZE72 = 0x48,
             OP_PUSHSIZE73 = 0x49,
             OP_PUSHSIZE74 = 0x4a,
             OP_PUSHSIZE75 = 0x4b,
-            
+
+            // The next byte is the number of following bytes to push
             OP_PUSHDATA1 = 0x4c,
+            // The next 2 bytes are the number of following bytes to push
             OP_PUSHDATA2 = 0x4d,
+            // The next 4 bytes are the number of following bytes to push
             OP_PUSHDATA4 = 0x4e,
+            // The number -1 is pushed to the stack
             OP_1NEGATE = 0x4f,
+            // Unassigned word which will mark script as invalid if executed
             OP_RESERVED = 0x50,
+            // For OP_1 and OP_TRUE, the number 1 is pushed to the stack.
             OP_1 = 0x51,
             OP_TRUE = OP_1,
+            // The number 2 is pushed to the stack
             OP_2 = 0x52,
+            // The number 3 is pushed to the stack
             OP_3 = 0x53,
+            // The number 4 is pushed to the stack
             OP_4 = 0x54,
+            // The number 5 is pushed to the stack
             OP_5 = 0x55,
+            // The number 6 is pushed to the stack
             OP_6 = 0x56,
+            // The number 7 is pushed to the stack
             OP_7 = 0x57,
+            // The number 8 is pushed to the stack
             OP_8 = 0x58,
+            // The number 9 is pushed to the stack
             OP_9 = 0x59,
+            // The number 10 is pushed to the stack
             OP_10 = 0x5a,
+            // The number 11 is pushed to the stack
             OP_11 = 0x5b,
+            // The number 12 is pushed to the stack
             OP_12 = 0x5c,
+            // The number 13 is pushed to the stack
             OP_13 = 0x5d,
+            // The number 14 is pushed to the stack
             OP_14 = 0x5e,
+            // The number 15 is pushed to the stack
             OP_15 = 0x5f,
+            // The number 16 is pushed to the stack
             OP_16 = 0x60,
 
             // control
+            // Does nothing
             OP_NOP = 0x61,
+            // Unassigned word which will mark script as invalid if executed
             OP_VER = 0x62,
+            // Clear top stack item, if true execute statements until OP_ENDIF
             OP_IF = 0x63,
+            // Clear top stack item, if false execute statements until OP_ENDIF
             OP_NOTIF = 0x64,
+            // Unassigned word which will mark script as invalid if executed
             OP_VERIF = 0x65,
+            // Unassigned word which will mark script as invalid if executed
             OP_VERNOTIF = 0x66,
+            // Execute until OP_ENDIF when previous IF / NOTIF not executed
             OP_ELSE = 0x67,
+            // End of IF statement required for valid script
             OP_ENDIF = 0x68,
+            // Clears the top stack value and marks script invalid if false
             OP_VERIFY = 0x69,
+            // Ends script interpretation and marks script invalid
             OP_RETURN = 0x6a,
 
             // stack ops
+            // Moves the current main stack top to alt stack
             OP_TOALTSTACK = 0x6b,
+            // Moves the current alt stack top to main stack
             OP_FROMALTSTACK = 0x6c,
+            // Removes the top two stack items
             OP_2DROP = 0x6d,
+            // Copy the top two stack items and place copies on stack
             OP_2DUP = 0x6e,
+            // Copy the top three stack items and place copies on stack
             OP_3DUP = 0x6f,
+            // Copy stack top - 2 & stack top - 3 to the top of the stack
             OP_2OVER = 0x70,
+            // Stack top - 4 & stack top - 5 are moved to the top of the stack
             OP_2ROT = 0x71,
+            // Swaps the top two stack values with the two values below them
             OP_2SWAP = 0x72,
+            // Duplicate the top stack value if it is non-zero / true
             OP_IFDUP = 0x73,
+            // Push the current number of stack values to the stack
             OP_DEPTH = 0x74,
+            // Remove the top stack item
             OP_DROP = 0x75,
+            // Duplicate the top stack item
             OP_DUP = 0x76,
+            // Remove stack top - 1
             OP_NIP = 0x77,
+            // Copy stack top - 1 to stack
             OP_OVER = 0x78,
+            // Copy stack top - N to the top where N is the current stack top
             OP_PICK = 0x79,
+            // Move stack top - N to the top where N is the current stack top
             OP_ROLL = 0x7a,
+            // Performs two swaps to rotate the top 3 stack values (123 -> 231)
             OP_ROT = 0x7b,
+            // Swap the top two stack items with eachother
             OP_SWAP = 0x7c,
+            // Copy top stack item to stack top - 1
             OP_TUCK = 0x7d,
 
             // splice ops
@@ -170,7 +220,7 @@ namespace abstractions::script {
             OP_NUM2BIN = 0x80, // after monolith upgrade (May 2018)
             OP_BIN2NUM = 0x81, // after monolith upgrade (May 2018)
             OP_SIZE = 0x82,
-            
+
             // bit logic
             OP_INVERT = 0x83,
             OP_AND = 0x84,
@@ -250,41 +300,41 @@ namespace abstractions::script {
 
             OP_INVALIDOPCODE = 0xff,
         };
-            
+
         struct noop;
         struct push;
         struct op_code;
         struct sequence;
         struct string;
         struct repeated;
-        
+
     };
-    
+
     struct program::noop : public program {
         void write(ostream&) const final override {}
-        
+
         N length() const final override {
             return 0;
         }
-        
+
         noop() {}
     };
-    
+
     struct program::push : public program {
         bytes Data;
-        
+
         void write(ostream& o) const final override {
             N size = Data.size();
             if (size == 0 || (size == 1 && Data[0] == 0)) {
                 o << program::OP_0;
                 return;
             }
-            
+
             if (size == 1 && Data[0] <= 16) {
                 o << Data[0] + 0x50;
                 return;
             }
-            
+
             if (size > 0xffff) {
                 o << program::OP_PUSHDATA4;
                 o << uint32(size);
@@ -296,11 +346,11 @@ namespace abstractions::script {
                 o << byte(size);
             } else {
                 o << byte(size);
-            } 
-            
+            }
+
             o << Data;
         }
-        
+
         N length() const final override {
             N size = Data.size();
             if (size == 0 || (size == 1 && Data[0] <= 16)) return 1;
@@ -309,61 +359,61 @@ namespace abstractions::script {
             if (size > 75) return size + 2;
             return size + 1;
         }
-        
+
         push(bytes data) : Data{data} {}
     };
-    
+
     struct program::op_code : public program {
         op OpCode;
         void write(ostream& o) const final override {
             o << byte(OpCode);
         }
-        
+
         N length() const final override {
             return 1;
         }
-        
+
         op_code(op o) : OpCode{o} {}
     };
-    
+
     struct program::string : public program {
         std::vector<op> String;
         void write(ostream& o) const final override {
             for (op code: String) o << byte(code);
         }
-        
+
         N length() const final override {
             return String.size();
         }
-        
+
         string(std::vector<op>& s) : String{s} {}
     };
-        
+
     struct program::sequence : public list<pointer<program>>, public program {
         using list<pointer<program>>::list;
         void write(ostream& o) const final override {
             for (pointer<program> p : *this) p->write(o);
         }
-        
+
         N length() const final override {
             return data::fold([](uint len, pointer<program> p)->uint{return len + p->length();}, 0, *this);
         }
     };
-    
+
     struct program::repeated : public program {
         N Repetitions;
         pointer<program> Repeated;
         void write(ostream& o) const final override {
             for (int i = 0; i < Repetitions; i++) Repeated->write(o);
         }
-        
+
         N length() const final override {
             return Repetitions * Repeated->length();
         }
-        
+
         repeated(N n, pointer<program> r) : Repetitions{n}, Repeated{r} {}
     };
-    
+
 }
 
 #endif
